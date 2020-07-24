b4927e9590fb985d55a0fc018e76e53310aa28fd
Stuart Maynes <stu@substrakt.com>
2020-07-24 21:03:41 +0100

    ADD: add_one function

--- chapter7-list-length.exs
+++ chapter7-list-length.exs
@@ -6,4 +6,8 @@ defmodule MyList do
   # Returns a list with the values of the orignal list squared
   def square([]), do: []
   def square([ head|tail ]), do: [ head*head | square(tail) ]
+
+  # Return a list with one added to each of the values
+  def add_one([]), do: []
+  def add_one([ head|tail ]), do: [ head+1 | add_one(tail) ]
 end
