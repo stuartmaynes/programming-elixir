e64a8d5cf02320d66e54d428a1497891b163030e
Stuart Maynes <stu@substrakt.com>
2020-07-24 21:00:40 +0100

    ADD: Sqaure function

--- chapter7-list-length.exs
+++ chapter7-list-length.exs
@@ -1,4 +1,9 @@
 defmodule MyList do
+  # Returns the length of the list param
   def len([]), do: 0
-  def len([_head|tail]), do: 1 + len(tail)
+  def len([ _head|tail ]), do: 1 + len(tail)
+
+  # Returns a list with the values of the orignal list squared
+  def square([]), do: []
+  def square([ head|tail ]), do: [ head*head | square(tail) ]
 end
